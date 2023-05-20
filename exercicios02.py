19a1ed932b4ff31e7402bf083fb206e72e2db80c
unknown <oshirothiago@gmail.com>
2023-05-20 15:39:56 -0300

    new exercises - conditionals, fixing enum of input exercises

--- exercicios02.py
+++ exercicios02.py
@@ -4,27 +4,64 @@
 #para requisitar dados externos ao programa:
 
 
-# 8 - Crie um algoritmo que ao ser executado receba o nome de um usuário 
+# 21 - Crie um algoritmo que ao ser executado receba o nome de um usuário 
 #e imprima esse nome
 
 
-# 9 - Crie um algoritmo que ao ser executado receba dois números de um usuário
+
+
+
+
+# 22 - Crie um algoritmo que ao ser executado receba dois números de um usuário
 # e imprima o resultado da soma entre esses dois números
 
 
-# 10 - Crie um algoritmo que ao ser executado receba dois números de um usuário
+
+
+
+
+
+# 23 - Crie um algoritmo que ao ser executado receba dois números de um usuário
 # e imprima o resultado da subtração entre esses dois números
 
 
-# 11 - Crie um algoritmo que ao ser executado receba dois números de um usuário
+
+
+
+
+
+# 24 - Crie um algoritmo que ao ser executado receba dois números de um usuário
 # e imprima o resultado da multiplicação entre esses dois números
 
 
-# 12 - Crie um algoritmo que ao ser executado receba dois números de um usuário
+
+
+
+
+# 25 - Crie um algoritmo que ao ser executado receba dois números de um usuário
 # e imprima o resultado da divisão entre esses dois números
 
 
-# 13 - Desafio: Crie um algoritmo que recebe dois valores númericos e qual operação 
+
+
+
+# 26 - Crie um algoritmo que ao ser executado receba um nome do usuário (através de um input), armazene essa informação numa variável
+# e imprima o valor do tipo dessa variável no terminal.
+
+
+
+
+#27 - Crie um algoritmo que ao sr executado receba um número (através de um input) e armaezene essa informação numa variável.
+# - Escreva em algum lugar qual tipo é o tipo dessa variável criada
+# - Imprima o valor do tipo dessa variável no terminal.
+# - Compare sua resposta com o resultado, foi o mesmo? Foi diferente? E se foi diferente, por que você acha que foi diferente?
+
+
+
+
+
+
+# 28 - Desafio: Crie um algoritmo que recebe dois valores númericos e qual operação 
 # o usuário deseja realizar e realize essa operação. Por exemplo:
 # O usuário forneceu as seguintes entradas: 4, 5, multiplicação.
 # Ele deseja multiplicar 4 e 5 e ser informado do resultado.
--- exercícios03.py
+++ exercícios03.py
@@ -11,6 +11,26 @@
 # e se for falso exiba a mensagem: "Olha que dia bonito, nem precisa levar guarda-chuva"
 
 
-# 10 - Crie um algoritmo que ao ser executado verficia: Se está ensolarado e se é fim de semana, 
+# 10 - Crie um algoritmo que ao ser executado verifica: Se está ensolarado e se é fim de semana, 
 #caso seja fim de semana E esteja ensolarado exiba a mensagem: "Bora pra praia de mineiro?" senão exiba a mensagem: "Bora ver um trem no Netflix?"
 
+
+# 11 - Crie um algoritmo que ao ser executado recebe a velocidade de dois veículos  em km/h e determina qual dos veículos está correndo
+# mais rápido exibe no terminal qual veículo está se deslocando mais rapidamente.
+# Se algum dos veículos estiver a mais de 80km/hora deve ser exibida a mensagem: "<veiculo> está acima da velocidade permitida"
+
+
+# 12 - Crie um algoritmo que ao ser executado recebe uma palavra, verifica se a palavra se inicia com uma vogal
+# e exibe a mensagem "Começa em vogal" caso a palavra se inice em vogal
+# ou exibe a mensagem "Começa em consoante" caso a palavra não se inicie em vogal
+
+# 13 - Crie um algoritmo que ao ser executado recebe uma frase se a frase inclui uma letra qualquer (a nossa escolha),
+# caso a frase contenha a letra escolhida deve ser exibida a mensagem "A letra <letra> está presente na frase"
+# caso contrário deve ser exibida a frase "A letra <letra> não está contida na frase"
+
+# 14 - Desafio: Crie um algoritmo para calcular a velocidade de um veículo
+# o calculo da velocidade é: velocidade = distância/tempo 
+# os valores serão recebidos através de inputs
+# Caso valores não númericos sejam recebidos o deve ser exibida uma mensagem de erro:
+# "Os valores inseridos não são números"
+# Caso valores corretos sejam informados o algoritmo deve imprimir o valor da velocidade calculada no terminal.
